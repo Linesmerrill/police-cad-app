64c5e303c177cc89641858724379d8f6fb9595fe
Merrill Lines <linesmerrill@gmail.com>
2023-02-08 14:42:14 -0700

    dismiss keyboard when not clicking on input box

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -10,6 +10,9 @@ import {
   StyleSheet,
   Dimensions,
   TextInput,
+  ScrollView,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 import React, { useEffect, useMemo } from "react";
 import {
@@ -42,160 +45,178 @@ const LoginScreen = () => {
     });
   }, []);
   return (
-    <View style={styles.container}>
-      <View style={styles.background}>
-        <Animated.View
-          style={[styles.backgroundViewWrapper, { opacity: opacity }]}
-        >
-          <Video
-            // isLooping
-            isMuted
-            positionMillis={500}
-            onLoad={() => {
-              Animated.timing(opacity, {
-                toValue: 1,
-                useNativeDriver: true,
-              }).start();
-            }}
-            resizeMode="cover"
-            // shouldPlay
-            source={require("../assets/royal-blue-bg-750.png")}
-            style={{ flex: 1 }}
-          />
-        </Animated.View>
-        <SafeAreaView className="h-full space-x-1" style={styles.overlay}>
-          <View className="flex-row ml-2">
-            <TouchableOpacity
-              className="shadow-sm"
-              onPress={() => navigation.navigate("Welcome")}
-            >
-              <ArrowLeftCircleIcon className="flex-1" color="white" size={40} />
-            </TouchableOpacity>
-          </View>
-
-          <View className="flex-row justify-center">
-            <Image
-              className="w-20 h-20"
-              source={require("../assets/favicon.png")}
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.container}>
+        <View style={styles.background}>
+          <Animated.View
+            style={[styles.backgroundViewWrapper, { opacity: opacity }]}
+          >
+            <Video
+              // isLooping
+              isMuted
+              positionMillis={500}
+              onLoad={() => {
+                Animated.timing(opacity, {
+                  toValue: 1,
+                  useNativeDriver: true,
+                }).start();
+              }}
+              resizeMode="cover"
+              // shouldPlay
+              source={require("../assets/royal-blue-bg-750.png")}
+              style={{ flex: 1 }}
             />
-          </View>
-
-          <View className="mt-4 flex-col p-4">
-            <View className="flex-row justify-center">
-              <Text className="text-3xl text-white">Welcome back.</Text>
-            </View>
-            <View className="flex-row justify-center">
-              <Text className="text-3xl text-white">
-                Log in and start roleplaying.
-              </Text>
-            </View>
-          </View>
-
-          <View className="mt-1">
-            <View className="flex-col space-x-1 pb-4">
-              <View className="flex-row">
-                <View className="flex-1 mx-5 mr-4 mt-6">
-                  <View className="bg-black p-4 rounded-lg border-solid border-2 border-gray-400">
-                    <TextInput
-                      placeholder="Email address"
-                      placeholderTextColor={"gray"}
-                      keyboardType="email-address"
-                    />
-                  </View>
-                </View>
-              </View>
-              <View className="flex-row">
-                <View className="flex-1 mx-4 mt-4">
-                  <View className="bg-black p-4 rounded-lg border-solid border-2 border-gray-400">
-                    <TextInput
-                      placeholder="Password"
-                      placeholderTextColor={"gray"}
-                      keyboardType="password"
-                    />
-                  </View>
-                </View>
-              </View>
-            </View>
-            <View className="flex-row">
+          </Animated.View>
+          <SafeAreaView className="h-full space-x-1" style={styles.overlay}>
+            <View className="flex-row ml-2">
               <TouchableOpacity
-                className="mx-5 bg-white p-4 mb-4 mr-4 rounded-full flex-1 items-center space-x-1"
-                onPress={() => navigation.navigate("Home")}
+                className="shadow-sm"
+                onPress={() => navigation.navigate("Welcome")}
               >
-                <View className="flex-row">
-                  <Text className=" text-black font-bold text-md text-center">
-                    Log in
-                  </Text>
-                </View>
+                <ArrowLeftCircleIcon
+                  className="flex-1"
+                  color="white"
+                  size={40}
+                />
               </TouchableOpacity>
             </View>
 
-            <View className="flex-row justify-center mb-4">
-              <Text className="text-white text-[15em]">
-                <Text
-                  style={{
-                    textDecorationLine: "underline",
-                    fontWeight: "bold",
-                  }}
-                  onPress={() => navigation.navigate("ForgotPassword")}
-                >
-                  Forgot your password?
+            <View className="flex-row justify-center">
+              <Image
+                className="w-20 h-20"
+                source={require("../assets/favicon.png")}
+              />
+            </View>
+
+            <View className="mt-4 flex-col p-4">
+              <View className="flex-row justify-center">
+                <Text className="text-3xl text-white">Welcome back.</Text>
+              </View>
+              <View className="flex-row justify-center">
+                <Text className="text-3xl text-white">
+                  Log in and start roleplaying.
                 </Text>
-              </Text>
-            </View>
-          </View>
-
-          <View className="mt-1">
-            <View className="flex-row">
-              <TouchableOpacity
-                className="mx-5 bg-[#007aff] p-4 mb-4 mr-5 rounded-full flex-1 items-center space-x-1"
-                onPress={() => navigation.navigate("Home")}
-              >
-                <View className="flex-row">
-                  <View className="absolute -left-24 -top-1">
-                    <RocketLaunchIcon color="white" size={25} />
-                  </View>
-                  <Text className=" text-white font-bold text-md text-center">
-                    Continue with Google
-                  </Text>
-                </View>
-              </TouchableOpacity>
+              </View>
             </View>
 
-            <View className="flex-row">
-              <TouchableOpacity
-                className="mx-5 bg-[#3c5997] p-4 mb-4 mr-5 rounded-full flex-1 items-center space-x-1"
-                onPress={() => navigation.navigate("Home")}
-              >
+            <View className="mt-1">
+              <View className="flex-col space-x-1 pb-4">
                 <View className="flex-row">
-                  <View className="absolute -left-24 -top-1">
-                    <BookOpenIcon color="white" size={25} />
+                  <View className="flex-1 mx-5 mr-4 mt-6">
+                    <View className="bg-black p-4 rounded-lg border-solid border-2 border-gray-400">
+                      <TextInput
+                        placeholder="Email address"
+                        placeholderTextColor={"gray"}
+                        keyboardType="email-address"
+                        cursorColor={"white"}
+                        autoComplete="email"
+                        color={"white"}
+                        focusable
+                      />
+                    </View>
                   </View>
-                  <Text className=" text-white font-bold text-md text-center">
-                    Continue with Facebook
-                  </Text>
                 </View>
-              </TouchableOpacity>
+                <View className="flex-row">
+                  <View className="flex-1 mx-4 mt-4">
+                    <View className="bg-black p-4 rounded-lg border-solid border-2 border-gray-400">
+                      <TextInput
+                        placeholder="Password"
+                        placeholderTextColor={"gray"}
+                        keyboardType="password"
+                        cursorColor={"white"}
+                        autoComplete="email"
+                        color={"white"}
+                        secureTextEntry
+                      />
+                    </View>
+                  </View>
+                </View>
+              </View>
+              <View className="flex-row">
+                <TouchableOpacity
+                  className="mx-5 bg-white p-4 mb-4 mr-4 rounded-full flex-1 items-center space-x-1"
+                  onPress={() => navigation.navigate("Home")}
+                >
+                  <View className="flex-row">
+                    <Text className=" text-black font-bold text-md text-center">
+                      Log in
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+
+              <View className="flex-row justify-center mb-4">
+                <Text className="text-white text-[15em]">
+                  <Text
+                    style={{
+                      textDecorationLine: "underline",
+                      fontWeight: "bold",
+                    }}
+                    onPress={() => navigation.navigate("ForgotPassword")}
+                  >
+                    Forgot your password?
+                  </Text>
+                </Text>
+              </View>
             </View>
 
-            <View className="flex-row">
-              <TouchableOpacity
-                className="mx-5 bg-[#fff] p-4 mr-5 rounded-full flex-1 items-center space-x-1"
-                onPress={() => navigation.navigate("Home")}
-              >
-                <View className="flex-row">
-                  <View className="absolute -left-28 -top-1">
-                    <GlobeAltIcon color="black" size={25} />
+            <View className="mt-1">
+              <View className="flex-row">
+                <TouchableOpacity
+                  className="mx-5 bg-[#007aff] p-4 mb-4 mr-5 rounded-full flex-1 items-center space-x-1"
+                  onPress={() => navigation.navigate("Home")}
+                >
+                  <View className="flex-row">
+                    <View className="absolute -left-24 -top-1">
+                      <RocketLaunchIcon color="white" size={25} />
+                    </View>
+                    <Text className=" text-white font-bold text-md text-center">
+                      Continue with Google
+                    </Text>
                   </View>
-                  <Text className=" text-black font-bold text-md text-center">
-                    Continue with Apple
-                  </Text>
-                </View>
-              </TouchableOpacity>
+                </TouchableOpacity>
+              </View>
+
+              <View className="flex-row">
+                <TouchableOpacity
+                  className="mx-5 bg-[#3c5997] p-4 mb-4 mr-5 rounded-full flex-1 items-center space-x-1"
+                  onPress={() => navigation.navigate("Home")}
+                >
+                  <View className="flex-row">
+                    <View className="absolute -left-24 -top-1">
+                      <BookOpenIcon color="white" size={25} />
+                    </View>
+                    <Text className=" text-white font-bold text-md text-center">
+                      Continue with Facebook
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+
+              <View className="flex-row">
+                <TouchableOpacity
+                  className="mx-5 bg-[#fff] p-4 mr-5 rounded-full flex-1 items-center space-x-1"
+                  onPress={() => navigation.navigate("Home")}
+                >
+                  <View className="flex-row">
+                    <View className="absolute -left-28 -top-1">
+                      <GlobeAltIcon color="black" size={25} />
+                    </View>
+                    <Text className=" text-black font-bold text-md text-center">
+                      Continue with Apple
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
             </View>
-          </View>
-        </SafeAreaView>
+          </SafeAreaView>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
