223cee72c63eb7bbd06826f19b4e856f47b0caa6
Merrill Lines <linesmerrill@gmail.com>
2023-04-23 07:37:51 -0700

    update create new account button

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -201,7 +201,7 @@ const WelcomeScreen = () => {
 
             <View className="flex-row">
               <TouchableOpacity
-                className="mx-5 bg-[#A62C17] p-4 mb-4 mr-5 rounded-full flex-1 items-center space-x-1"
+                className="mx-5 bg-[#13cb10] p-4 mb-4 mr-5 rounded-full flex-1 items-center space-x-1"
                 onPress={() => navigation.push("CreateAccount")}
               >
                 <View className="flex-row">
