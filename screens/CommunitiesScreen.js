e3f007f3aa7802fe20a9fd5ff66239caeb0c304b
Merrill Lines <linesmerrill@gmail.com>
2024-02-08 11:29:29 -0700

    redo community page

--- screens/CommunitiesScreen.js
+++ screens/CommunitiesScreen.js
@@ -1,49 +1,11 @@
-import {
-  View,
-  Text,
-  SafeAreaView,
-  Image,
-  TouchableOpacity,
-  Animated,
-} from "react-native";
-import React, { useLayoutEffect, useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import {
-  ChevronDownIcon,
-  HomeIcon,
-  UserIcon,
-} from "react-native-heroicons/outline";
-import UserCategory from "../components/RoleIcon";
-import RoleIcon from "../components/RoleIcon";
+import { View, Text } from "react-native";
+import React from "react";
 
 const CommunitiesScreen = () => {
-  const navigation = useNavigation();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, []);
-
   return (
-    <>
-      <SafeAreaView className="bg-[#02284F] pt-5">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 justify-between">
-          <TouchableOpacity onPress={navigation.goBack}>
-            <HomeIcon className="flex-1" size={35} color="#1B96D6" />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => navigation.navigate("Account")}>
-            <UserIcon className="flex-1" size={35} color="#1B96D6" />
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-
-      <SafeAreaView>
-        <Text>Communities</Text>
-      </SafeAreaView>
-    </>
+    <View>
+      <Text>CommunitiesScreen</Text>
+    </View>
   );
 };
 
