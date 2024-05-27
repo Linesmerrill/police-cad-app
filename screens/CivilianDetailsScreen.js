c9c86a4d9cf9726b65b1c58e7777561883d88c6c
Merrill Lines <linesmerrill@gmail.com>
2024-05-27 16:01:45 -0700

    update to latest SDK for expo

--- screens/CivilianDetailsScreen.js
+++ screens/CivilianDetailsScreen.js
@@ -7,11 +7,20 @@ import {
   SafeAreaView,
   StyleSheet,
   RefreshControl,
+  Modal,
+  Dimensions,
 } from "react-native";
 import React, { useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { EyeIcon } from "react-native-heroicons/solid";
-import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { EyeIcon, XMarkIcon } from "react-native-heroicons/solid";
+import {
+  ChevronLeftIcon,
+  PencilIcon,
+  PencilSquareIcon,
+} from "react-native-heroicons/outline";
+import { TextInput } from "react-native";
+import { Button } from "react-native";
+import { Animated } from "react-native";
 
 const CivilianDetailsScreen = () => {
   const navigation = useNavigation();
@@ -19,6 +28,34 @@ const CivilianDetailsScreen = () => {
   const [isPressed, setIsPressed] = useState(false);
   const [currentRole, setCurrentRole] = useState("Civilian");
   const [refreshing, setRefreshing] = React.useState(false);
+
+  const [text, setText] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  const slideAnim = useState(
+    new Animated.Value(-Dimensions.get("window").height)
+  )[0];
+
+  const handleEdit = () => {
+    setModalVisible(true);
+  };
+
+  const handleSave = () => {
+    setModalVisible(false);
+  };
+
+  const openModal = () => {
+    setModalVisible(true);
+    Animated.timing(slideAnim, {
+      toValue: -Dimensions.get("window").height * 0,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+    slideAnim.setValue(Dimensions.get("window").height);
+  };
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -63,10 +100,18 @@ const CivilianDetailsScreen = () => {
           {/* image rounded in the middle */}
           <View className="flex-row justify-center">
             <View className="flex-col items-center">
-              <Image
-                source={require("../assets/images/gradient-bg1.jpg")}
-                className="w-28 h-28 rounded-full"
-              />
+              {/* make image clickable so people can upload their own profile pic, also show a pencil icon */}
+
+              <TouchableOpacity>
+                <Image
+                  source={require("../assets/images/civ-selfie-1.png")}
+                  className="w-28 h-28 rounded-full"
+                />
+                {/* add blue circle background around pencil icon */}
+                <View className="flex-row bg-blue-500 rounded-full p-2 items-center absolute bottom-0 right-0">
+                  <PencilIcon size={20} className="text-white" color={"#FFF"} />
+                </View>
+              </TouchableOpacity>
               <View className="flex-row justify-around">
                 <Text
                   numberOfLines={1}
@@ -91,12 +136,71 @@ const CivilianDetailsScreen = () => {
           <View className="flex-row justify-around mt-4">
             <View className="flex-col space-y-2">
               <View className="flex-row justify-between mx-4">
-                <TouchableOpacity className="flex-row w-full justify-between">
+                <TouchableOpacity
+                  className="flex-row w-full justify-between"
+                  onPress={openModal}
+                >
                   <Text className="text-gray-500 text-lg font-semibold">
                     First Name
                   </Text>
 
-                  <Text className="text-lg font-bold text-white">Rodger</Text>
+                  {/* quarter screen modal */}
+
+                  <Modal
+                    visible={modalVisible}
+                    animationType="none"
+                    transparent={true}
+                    onRequestClose={() => {
+                      setModalVisible(false);
+                    }}
+                  >
+                    <TouchableOpacity onPress={closeModal}>
+                      <View style={styles.overlay} />
+                    </TouchableOpacity>
+
+                    <Animated.View
+                      style={[styles.modal, { bottom: slideAnim }]}
+                      className="rounded-lg bg-white p-4"
+                    >
+                      <TouchableOpacity
+                        //   style={styles.overlay}
+                        onPress={closeModal}
+                      >
+                        <XMarkIcon
+                          color={"#FFF"}
+                          style={{
+                            position: "absolute",
+                            top: 2,
+                            right: 2,
+                          }}
+                        />
+                      </TouchableOpacity>
+                      <Text className=" text-gray-300">First Name</Text>
+                      <TextInput
+                        value={text}
+                        onChangeText={setText}
+                        style={styles.input}
+                      />
+                      {/* styled save button with background of light blue*/}
+                      <TouchableOpacity onPress={handleSave}>
+                        <View className="bg-blue-500 rounded-lg p-2 items-center">
+                          <Text className="text-white text-md font-bold p-2">
+                            Save
+                          </Text>
+                        </View>
+                      </TouchableOpacity>
+
+                      {/* <Button title="Save" onPress={handleSave} /> */}
+                    </Animated.View>
+                  </Modal>
+
+                  <Text
+                    className="text-lg font-bold text-white"
+                    value={text}
+                    onChangeText={setText}
+                  >
+                    {text}
+                  </Text>
                   {/* horizontal line */}
                 </TouchableOpacity>
               </View>
@@ -165,6 +269,27 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     // alignItems: "center",
     backgroundColor: "black",
+  },
+  input: {
+    // width: 100,
+    height: 40,
+    borderColor: "gray",
+    borderBottomWidth: 1,
+    marginBottom: 50,
+    padding: 10,
+    color: "white",
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modal: {
+    position: "absolute",
+    width: "100%",
+    height: "25%",
+    backgroundColor: "#222",
+    text: "white",
+    bottom: 0,
   },
 });
 
